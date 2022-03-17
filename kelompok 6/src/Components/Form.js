326108660fec795440c5c97c55bc7694fa055d62
Muhammad Farizul Imami Sudirman <ozuxekyosuke@gmail.com>
2022-03-17 19:51:59 +0700

    FormLogin

---
+++ kelompok 6/src/Components/Form.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import './CSS/todo.css'
+
+class Form extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            firstName: "",
+            password: "",        
+        }
+        this.handleSubmit=this.handleSubmit.bind(this)
+    }
+
+    firsthandler = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+   
+    }
+    passwordhandler = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+    
+    handleSubmit = (event) => {
+        alert(`${this.state.firstName}  Login Berhasil !`)
+        console.log(this.state);
+        this.setState({
+            firstName: "",
+        
+            password: '',
+          
+        })
+     event.preventDefault()
+        
+    }
+    render() {
+        return (
+            <div>
+
+                <form onSubmit={this.handleSubmit}>
+                    <h1>Login</h1>
+                     <input type="text" value={this.state.firstName} onChange={this.firsthandler} placeholder="UserName" /><br />
+                
+                     <input type="password" value={this.state.password} onChange={this.passwordhandler} placeholder="Password" /><br />
+                   
+                    <input type="submit" value="Submit" />
+                </form>
+
+            </div>
+            
+        )
+    }
+}
+
+export default Form
